1e0936a7a10a13d197bc3fa11e3a5df09934873f
Ethan Hall <ethanhall99@gmail.com>
2021-03-16 18:49:20 -0500

    v2

--- forms/newControls/newControls.js
+++ forms/newControls/newControls.js
@@ -1,6 +1,6 @@
 
 HMBRleft.onclick=function(s1){
-  if(typeof(s) == "object") {
+  if(typeof(s1) == "object") {
     return
   } else {
       switch(s1) {
@@ -17,18 +17,50 @@ HMBRleft.onclick=function(s1){
 }
 
 DDmiddle.onclick=function(s2){
-  if(typeof(s) == "object") {
+  if(typeof(s2) == "object") {
     return
   } else {
       switch(s2) {
         case "Calculator":
-            HMBRleft.hide()
             ChangeForm(calculator)
             break
         case "Membership":
-            HMBRleft.hide()
             ChangeForm(membership)
             break
       }
   }   
+}
+
+RDBswitch.onclick=function(){
+      if (RDBswitch.value == 0)
+        NSB.MsgBox("Use Dropdown to switch to pages")
+      else 
+        NSB.MsgBox("Use Dropdown to switch to pages")  
+}
+
+
+let form = 'Calculator'
+
+SELECTform.onchange=function(){
+      if (SELECTform.value == 1)
+        form = 'Calculator'
+      else if (SELECTform.value == 2)
+        form = 'Membership'  
+      else 
+        form = 'newControls'
+}
+
+
+BTNgo.onclick=function(){
+      switch(form) {
+        case "Calculator":
+            ChangeForm(calculator)
+            break
+        case "Membership":
+            ChangeForm(membership)
+            break
+        case "newControls":
+            ChangeForm(newControls)
+            break            
+      }
 }
